ff13b3b5d7c363b61c6c1f7304227a8d1ce3c9e8
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2023-12-12 11:25:23 +0530

    basic routing setup using react-router-dom

---
+++ src/components/MainApp/MainAppContainer.js
@@ -0,0 +1,12 @@
+
+
+const MainAppContainer = (props) => {
+
+  return (
+    <div>
+      <h1>Hii</h1>
+    </div>
+  )
+}
+
+export default MainAppContainer
---
+++ src/components/login/LoginContainer.js
@@ -0,0 +1,10 @@
+
+const Login = (props) => {
+  return (
+    <div>
+      hII
+    </div>
+  )
+}
+
+export default Login
