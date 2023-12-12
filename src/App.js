7f2b8cc41dde1f9e6683780aec500933c9eaddb4
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2023-12-12 12:17:25 +0530

    login form ui setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { useState } from 'react';
 
 import Login from "./components/login/LoginContainer";
 import MainAppContainer from './components/MainApp/MainAppContainer';
 
+const App = (props) => {
+  const [data, setData] = useState({
+    users: [
+      {
+        "id": 1,
+        "username": "amar",
+        "password": "amar123"
+      },
+      {
+        "id": 2,
+        "username": "akbar",
+        "password": "akbar123"
+      },
+      {
+        "id": 3,
+        "username": "antony",
+        "password": "antony123"
+      },
+      {
+        "id": 4,
+        "username": "john",
+        "password": "john123"
+      },
+      {
+        "id": 5,
+        "username": "paul",
+        "password": "paul123"
+      }
+    ],
+    dishes: []
+  })
 
-const App = () => {
+  console.log(data);
+
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path='/' element={<Login />} />
-        <Route path='/main' element={<MainAppContainer />} />
+        <Route path='/' element={<Login data={data} />} />
+        <Route path='/main' element={<MainAppContainer data={data} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/login/LoginContainer.js
+++ src/components/login/LoginContainer.js
@@ -1,9 +1,44 @@
+import { Box, Typography, TextField, Button, Stack } from "@mui/material"
+import { useState } from "react"
 
 const Login = (props) => {
+  const [formData, setFormData] = useState({ username: '', password: '' })
+
+  const loginHandleFunction = (e) => {
+    e.preventDefault()
+
+    console.log(formData);
+  }
+
   return (
-    <div>
-      hII
-    </div>
+    <Box>
+      <Typography variant="h3">Login</Typography>
+      <Box component="form" onSubmit={loginHandleFunction}>
+        <Stack justifyContent='center' width="50vw">
+          <TextField
+            label="username"
+            variant="outlined"
+            type='text'
+            value={formData.username}
+            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+            // error={formik.errors.email && true}
+            // helperText={formik.errors.email}
+            sx={{ backgroundColor: "white" }} />
+
+          <TextField
+            label="password"
+            variant="outlined"
+            type='password'
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+            // error={formik.errors.email && true}
+            // helperText={formik.errors.email}
+            sx={{ backgroundColor: "white" }} />
+
+          <Button type="submit" variant="contained">Login</Button>
+        </Stack>
+      </Box>
+    </Box>
   )
 }
 
